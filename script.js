d124070a02813d6a8c23b18c2e28707afb659639
karl <karl.clemence@outlook.fr>
2024-12-08 20:15:04 +0100

    Maj Dimanche

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 alert("Hello World");
+
+
+
+function toggleMenu() {
+    const menu = document.querySelector('.menu ul');
+    menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
+}
